31768a4bfaa54716242f2e5cfc6ebfa7db621b19
Przemyslaw Pilipczuk <3.14lipczuk@gmail.com>
2024-12-23 00:44:37 +0100

    job def

--- reproduce.py
+++ reproduce.py
@@ -8,9 +8,6 @@ but a nicer way to have whole analysis done is to run this one script and have t
 I utilize the slurm job dependency mechanism to schedule all the jobs, and set correct dependencies between them, so that they are executed in correct sequence.
 """
 
-def main():
-    pass
-
 """
 Design question:
 Is it better to make:
@@ -24,17 +21,45 @@ I feel like the way to go is to design this in a way where i define jobs in orde
 
 @dataclass
 class Job:
-    path:str
-    children:list[Job]
+    path:str 
+    children:list[Job] = []
 
     def depends_on(self, parent):
         parent.children.append(self)
 
-    def run(self):
-        # TODO: make this run the sbatch w/ subprocess
-        pass
+    def run(self, run_by=None):
+        dep = [f'--dependency=afterok:{run_by}'] if run_by is not None else []
+        sbatch_command = ['sbatch', *dep, self.path ]
+        job = subprocess.run(sbatch_command, capture_output=True)
+        assert(job.returncode == 0)
+        job_id = job.stdout.strip().replace('\n','')
+        for child in self.children:
+            child.run(run_by=job_id)
+            
 
 # TODO: add paths to scripts and make sure all steps are in them (there is no 'glue' that i wrote in terminal at the time)
+# TODO: make some kind of basescript/config.sh with bash variables that will be sourced into every step of the pipeline. This will make it nicer for anyone who would want to repro it later, as they dont have to change every single path in every script, just one path in the base script.
+# TODO: fucking get_samplelist wants an argument. god damnit. I should've written it correctly the first time around.
+
+# Job definition
+fastqc = Job("./scripts/fastqc.sh")
+get_samplelist = Job("./scripts/get_samplelist.sh")
+multiqc = Job("./scripts/multiqc.sh")
+make_idx = Job("./mapping/1-make_index.sh")
+map_reads = Job("./mapping/2-map_reads.sbatch")
+mapping_qc = Job("./mapping/multiqc.sh")
+feature_count = Job("./feature_counts/featureCount.sbatch")
+
+# Dependency definition
+get_samplelist.depends_on(fastqc)
+multiqc.depends_on(fastqc)
+make_idx.depends_on(multiqc)
+map_reads.depends_on(make_idx)
+mapping_qc.depends_on(map_reads)
+feature_count.depends_on(mapping_qc)
+
+def main():
+    get_samplelist.run()
 
 if __name__ == '__main__':
     main()
